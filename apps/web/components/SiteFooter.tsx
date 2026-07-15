cf5da0303fcdf0f33d38003bb3e077fb905cac10
M-Osugi1230 <osugimurata@icloud.com>
2026-07-15 11:36:23 +0900

    Link the executive registry from the footer

--- apps/web/components/SiteFooter.tsx
+++ apps/web/components/SiteFooter.tsx
@@ -10,6 +10,7 @@ export function SiteFooter() {
       <div className="footerLinks">
         <Link href="/about">このサイトの目的</Link>
         <Link href="/compare">都市比較</Link>
+        <Link href="/executives">首長台帳</Link>
         <Link href="/assemblies">議会アカウンタビリティ</Link>
         <Link href="/methodology">方法論</Link>
         <Link href="/data-quality">データ品質</Link>
